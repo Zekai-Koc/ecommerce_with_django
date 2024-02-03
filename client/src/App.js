cd2d857b881abd515315233cd82d79abc1c83da7
Zekai Koç <zekaikoc@gmail.com>
2024-02-04 00:54:43 +0100

    frontend added.

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+   return (
+      <div>
+         <Header />
+
+         <main>
+            <h1>Welcome to my Ecommerce Site...</h1>
+         </main>
+         <Footer />
+      </div>
+   );
+}
+
+export default App;
