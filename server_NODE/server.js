ba8529ec9122bb2f42a240e4569de387d193f94f
Zekai Koç <zekaikoc@gmail.com>
2024-08-07 22:57:16 +0200

    postgres connection.

--- server_NODE/server.js
+++ server_NODE/server.js
@@ -1,11 +1,44 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+const sequelize = require("sequelize");
 
 const app = require("./app");
 
 console.log(app.get("env"));
 // console.log(process.env);
 console.log(process.env.DB_USER);
+
+const config = {
+   username: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   dialect: "postgres",
+};
+
+const db = (module.exports = new sequelize({
+   ...config,
+   logging: false,
+   define: {
+      underscored: true,
+      freezeTableName: true,
+      timestamps: true,
+      charset: "utf-8",
+      dialectOptions: { collate: "utf-8_general_ci" },
+   },
+}));
+
+const testConnection = async () => {
+   try {
+      await db.authenticate();
+      console.log("Connection has been established successfully.");
+   } catch (error) {
+      console.error("Unable to connect to the database:", error);
+   }
+};
+
+testConnection();
 
 const port = process.env.PORT || 3000;
 
